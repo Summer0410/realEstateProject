2ba9a2a30afbdb5465021a165eb6b1efbdc75a72
Linqiao Xia <linqiaoxia@Linqiaos-MacBook-Air.local>
2019-05-26 21:23:58 -0230

    customized admin page

--- realtors/models.py
+++ realtors/models.py
@@ -5,7 +5,7 @@ class Realtor(models.Model):
     name = models.CharField(max_length = 200)
     photo = models.ImageField(upload_to = 'photo/%Y/%m/%d/')
     description = models.TextField(blank =True)
-    photo = models.CharField(max_length = 20)
+    photo = models.ImageField(max_length = 20)
     email = models.CharField(max_length = 50)
     is_mvp = models.BooleanField(default = False)
     hire_date = models.DateTimeField(default = datetime.now, blank = True)
