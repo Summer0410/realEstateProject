2ffb814ef0f773c1c433a264a5f06ba2069ac0b3
Linqiao Xia <summerxialinqiao@gmail.com>
2020-03-02 22:47:34 -0330

    added register logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render, redirect
 from accounts.forms import registerForm, LoginForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        print("It worked")
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        #check if passwords match
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                print("user exist")
+                return redirect('accounts/register.html')
+            else:
+                if User.objects.filter(email=email).exists():
+                    print("email has been used")
+                    return redirect('accounts/register.html')
+                else:
+                    user = User.objects.create_user(username = username, email= email, first_name  = first_name, last_name = last_name,password=password)
+                    user.save()
+                    return render(request, 'accounts/login.html')
     return render(request, 'accounts/register.html')
 def login(request):
     return render(request, 'accounts/login.html')
--- lxre/settings.py
+++ lxre/settings.py
@@ -152,3 +152,10 @@ STATICFILES_DIRS = [
 #Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+#message system
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
